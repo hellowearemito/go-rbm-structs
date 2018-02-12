d196439555a85244a0db6b376cb9b8d5907db985
Attila Sumi <a.sumi@mito.hu>
2018-02-12 12:55:46 +0100

    add filename to suggestion message type

--- send/message.go
+++ send/message.go
@@ -18,6 +18,7 @@ type BaseMessage struct {
 // SuggestionMessage represents the suggestion message structure.
 type SuggestionMessage struct {
 	Text     string           `json:"text,omitempty"`
+	FileName string           `json:"fileName,omitempty"`
 	RichCard *RichCardMessage `json:"richCard,omitempty"`
 	BaseMessage
 }
